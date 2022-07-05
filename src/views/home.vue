f2e122dbd89b9adfa254acef3ccd7134df910377
Anthony <824283752@qq.com>
2022-07-05 14:47:23 +0800

    增加vue-router的基础使用

---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-wrapper">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/vuex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vuex-wrapper">
+    Vuex
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vuex"
+}
+</script>
+
+<style scoped>
+
+</style>
