ce71d370f8f3d21c41505ccd0dc8b7763db28b3f
Anthony <824283752@qq.com>
2022-07-05 14:53:21 +0800

    增加elementplus ui的使用

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router/index'
+import store from './store/index'
 
-createApp(App).use(router).mount('#app')
+// elementPlus UI
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
